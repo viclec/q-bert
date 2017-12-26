4a6e0553ee79006d0455c2f596dc4667127cb428
alexispa <pavlakisalexandros@gmail.com>
2017-12-26 15:58:28 +0200

    Add files via upload
    change colors on pyramid

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
 #include <allegro5\allegro_native_dialog.h>
-
+#include <vector>
+#include <cmath>
 
 const int WIDTH = 800;
 const int HEIGHT = 640;
@@ -22,21 +23,66 @@ double cubeY=180;
 double cubeSizeX = 32;
 double cubeSizeY = 38;
 
+struct pyramid{
+	unsigned x;
+	unsigned y;
+	unsigned multiplier;
+};
+
 bool keys[] = {false, false, false, false};
 enum KEYS{UP, DOWN, LEFT, RIGHT};
 enum CHARACTERS{QBERT, SNAKE, BALL, DISK, ISO};
 
 int h1 = 7;
 int h3 = 26;
+bool is_first_time = true;
 
 double iso_cube_y (double row){
 	return WIDTH/4 + (row-1)*h3;
 }
 
-void Compute_iso_cube_placement(double row,double col){
+void pyramid_colision(Sprites qbert, std::vector<pyramid> &p)
+{	
+	//########################
+	// bound width, height
+	//########################
+	unsigned counter = 0;
+	unsigned q_x = qbert.getPositionX();
+	unsigned q_y = qbert.getPositionY();
+	unsigned q_bx = qbert.getFrameWidth();			
+	unsigned q_by = qbert.getFrameHeight();
+	int diff = HEIGHT;
+	int position = -1;
+	
+	while(counter < p.size())
+	{
+		if(p[counter].x == q_x)
+		{
+			if(p[counter].y - q_y  <= 0){
+				counter++;
+				continue;
+			}
+			if(p[counter].y - q_y <= diff)
+			{
+				diff = (int)std::abs((int)(q_y - p[counter].y));
+				position = counter;
+			}
+		}
+		counter++;
+	}
+	if(position >= 0)
+	{
+		p[position].multiplier = 7;
+	}
+//	assert(position >= 0);
+}
+
+//otan pathsw sto cube pollaplasiazw to cubeX me 6.6 gia na alla3w xrwma
+void Compute_iso_cube_placement(double row,double col, std::vector<pyramid> &pyramid, bool &first_run){
 	double i;
 	double j;
-
+	int index = 0;
+	struct pyramid p;
 	for(i=0; i<row; i++){
 
 		double y = iso_cube_y(i);
@@ -47,15 +93,28 @@ void Compute_iso_cube_placement(double row,double col){
 			double x = xs + (j-1)*cubeSizeX;
 			double xc = x + cubeSizeX/2;
 			double yc = y + h1;
-			al_draw_bitmap_region(bitmap, cubeX , cubeY, cubeSizeX, cubeSizeY,xc, yc , 0);
+
+			if(first_run){
+				p.x = xc;
+				p.y = yc;
+				p.multiplier = 1;
+				pyramid.push_back(p);
+			}
+
+			al_draw_bitmap_region(bitmap, pyramid[index].multiplier * cubeX , cubeY, cubeSizeX, cubeSizeY,xc, yc , 0);	
+			index++;
 		}
 	}	
+	
+	first_run = false;
 }
 
 int main()
 {	//==============================================
 	// PROJECT VARIABLES
 	//==============================================
+	std::vector<pyramid> pyramid_boxes;
+	bool first_run = true;
 	bool done = false;
 	bool render = false;
 	const float FPS = 60.0;
@@ -137,7 +196,7 @@ int main()
 		10, 10,
 		qbertFrames, 5,
 		1, 10, 1, 0, 
-		1, 303, 145);
+		1, 304, 145);
 
 	Sprites ball(bitmap, spriteHeightInBitmap[BALL], ballX, ballY,
 		10, 10, 
@@ -216,6 +275,13 @@ int main()
 		else if(ev.type == ALLEGRO_EVENT_TIMER)
 		{
 			render = true;
+			unsigned qx = qbert.getPositionX();
+			unsigned qy = qbert.getPositionY();
+			if(!first_run)
+			{
+				pyramid_colision(qbert,pyramid_boxes);
+			}
+			
 
 		}
 
@@ -225,12 +291,14 @@ int main()
 		if(render && al_is_event_queue_empty(event_queue))
 		{
 			render = false;
-			Compute_iso_cube_placement(12,3);
+			
+			Compute_iso_cube_placement(12,3, pyramid_boxes, first_run);
+			
 			ball.Draw();
 			ball.animationUpdate();
 			qbert.Draw();
 			qbert.animationUpdate();
-
+			
 			al_flip_display();
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 		}	
